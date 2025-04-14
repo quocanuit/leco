9b892d6eaa58f6edbf9c186a211c9e3dc9865e67
quocanuit <quocanuit@gmail.com>
2025-04-14 16:36:44 +0000

    fix: cannot pickle '_thread.RLock' object

--- src/rag/vectorstore.py
+++ src/rag/vectorstore.py
@@ -9,35 +9,31 @@ load_dotenv()
 class VectorDB:
     def __init__(self,
                 documents=None,
-                vector_db=Qdrant,
                 embedding=HuggingFaceEmbeddings(model_name="BAAI/bge-m3"),
                 collection_name="judgment_collection",
                 location=os.getenv("VECTOR_DB_URL"),
                 client=None
             ) -> None:
 
-        self.vector_db = vector_db
         self.embedding = embedding
         self.collection_name = collection_name
         self.location = location
-        self.client = client
+        self.client = client if client else QdrantClient(url=location)
         self.db = self._build_db(documents)
 
     def _build_db(self, documents):
-        client = self.client if self.client else QdrantClient(url=self.location)
-        
         if documents is None:
-            db = self.vector_db(
-                client=client,
+            db = Qdrant(
+                client=self.client,
                 collection_name=self.collection_name,
                 embeddings=self.embedding
             )
         else:
-            db = self.vector_db.from_documents(
+            db = Qdrant.from_documents(
                 documents=documents,
                 embedding=self.embedding,
                 collection_name=self.collection_name,
-                client=client
+                url=self.location
             )
         
         return db
